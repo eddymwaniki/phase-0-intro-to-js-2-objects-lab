fe959fd1b63ae719c516ca3ab516a82485adeac6
eddy <eddy.mwaniki.27@gmail.com>
2024-12-08 02:27:52 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name : "Laban Daron",
+    streetAddress : "Nairobi"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, { [key]: value });
+}
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "Age",31)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = Object.assign({}, employee);
+  
+    delete newEmployee[key];
+  
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  
+    return employee;
+  }
+
+
+
+
